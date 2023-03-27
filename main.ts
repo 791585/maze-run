61cd0845fd6a1f01f96bb0b59f84fbc6094d7be1
paige <127938204+791585@users.noreply.github.com>
2023-03-27 15:32:04 +0100

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -4,9 +4,8 @@ scene.onOverlapTile(SpriteKind.Player, assets.tile`myTile18`, function (sprite, 
 scene.onOverlapTile(SpriteKind.Player, assets.tile`myTile17`, function (sprite, location) {
     info.changeLifeBy(2)
 })
-let mySprite: Sprite = null
 scene.setBackgroundImage(assets.image`myImage0`)
 tiles.setCurrentTilemap(tilemap`level`)
 let squeaks = sprites.create(assets.image`squeaks`, SpriteKind.Player)
 controller.moveSprite(squeaks, 100, 100)
-tiles.placeOnTile(mySprite, tiles.getTileLocation(1, 0))
+tiles.placeOnTile(squeaks, tiles.getTileLocation(1, 0))
